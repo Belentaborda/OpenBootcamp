97fc6469944ba5a372c1c3acaaba1bc9c8b46752
Maria Belen Taborda <116043357+Belentaborda@users.noreply.github.com>
2023-01-16 21:53:15 -0300

    Add files via upload

---
+++ opbootcamp_ej3/Opbootcamp_ej3.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package opbootcamp_ej3;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Opbootcamp_ej3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       String[] nombres = {"Paula", "Johana", "Cristian" , "Luz"};
+        System.out.println("Los nombres elegidos fueron: ");
+        for (String nombre : nombres) {
+            System.out.println( nombre );
+            
+        } 
+    } 
+    
+}
